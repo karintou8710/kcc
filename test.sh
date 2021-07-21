01f15a39f67892795402bcd67f682ebe0eb4d834
karintou8710 <karintou8710@gmail.com>
2021-07-21 14:24:37 +0900

    整数1つをコンパイルするコンパイラを作成

---
+++ test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# 引数はexpected, inputの順 
+assert() {
+    expected="$1"
+    input="$2"
+
+    ./9cc "$input" > tmp.s
+    cc -o tmp tmp.s
+    ./tmp
+    actual="$?"
+
+    if [ "$actual" = "$expected" ]; then
+        echo "$input => $actual"
+    else
+        echo "$input => $expected expected, but got $actual"
+        exit 1
+    fi
+}
+
+for i in 0 10 20 30 40 50
+do
+    assert $i $i
+done
+
+echo OK
