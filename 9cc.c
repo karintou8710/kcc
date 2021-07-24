3c5cd13941189e8653a4d953e009c96bf0dd23ba
karintou8710 <karintou8710@gmail.com>
2021-07-24 15:45:51 +0900

    トークナイズの処理を追加

--- 9cc.c
+++ 9cc.c
@@ -1,5 +1,100 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+    TK_RESERVED,
+    TK_NUM,
+    TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+    TokenKind kind;
+    Token *next;
+    int val;
+    char *str;
+};
+
+Token *token;
+
+void error(char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(EXIT_FAILURE);
+}
+
+// 演算子か判定
+bool consume(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] != op) {
+        return false;
+    }
+    token = token->next;
+    return true;
+}
+
+void expect(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] != op) {
+        error("'%c'ではありません", op);
+    }
+    token = token->next;
+}
+
+int expect_number() {
+    if (token->kind != TK_NUM) {
+        error("数ではありません");
+    }
+    int val = token->val;
+    token = token->next;
+    return val;
+}
+
+bool at_eof() {
+    return token->kind == TK_EOF;
+}
+
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+    Token *tok = calloc(1, sizeof(Token));
+    tok->kind = kind;
+    tok->str = str;
+    cur->next = tok;
+    return tok;
+}
+
+Token *tokenize(char *p) {
+    Token head;
+    head.next = NULL;
+    Token *cur = &head;
+
+    while (*p) {
+        if (isspace(*p)) {
+            p++;
+            continue;
+        }
+
+        if (*p == '+' || *p == '-') {
+            cur = new_token(TK_RESERVED, cur, p++);
+            continue;
+        }
+
+        if (isdigit(*p)) {
+            cur = new_token(TK_NUM, cur, p);
+            cur->val = strtol(p, &p, 10);
+            continue;
+        }
+
+        error("トークナイズできません");
+    }
+
+    new_token(TK_EOF, cur, p);
+    return head.next;
+}
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -7,26 +102,25 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
-    char *p = argv[1];
+    token = tokenize(argv[1]);
 
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
-    printf("  mov rax, %ld\n", strtol(p, &p, 10));
-    while (*p) {
-        if (*p == '+') {
-            p++;
-            printf("  add rax, %ld\n", strtol(p, &p, 10));
+    printf("  mov rax, %d\n", expect_number());
+    while (!at_eof()) {
+        if (consume('+')) {
+            int val = expect_number();
+            printf("  add rax, %d\n", val);
             continue;
         }
 
-        if (*p == '-') {
-            p++;
-            printf("  sub rax, %ld\n", strtol(p, &p, 10));
+        if (consume('-')) {
+            int val = expect_number();
+            printf("  sub rax, %d\n", val);
             continue;
         }
 
-        fprintf(stderr, "予期しない文字です: '%c'\n", *p);
         return EXIT_FAILURE;
     }
     printf("  ret\n");
--- test.sh
+++ test.sh
@@ -21,5 +21,6 @@ assert() {
 assert 1 1
 assert 3 3
 assert 21 "5+20-4"
+assert 41 " 12 + 34 - 5 "
 
 echo OK
