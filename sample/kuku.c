54854066bb569c360952ed8236dd4ad8a90d87ea
karintou8710 <karintou8710@gmail.com>
2022-03-07 01:15:43 +0900

    Add kuku sample

---
+++ sample/kuku.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    for (int i=1;i<10;i++) {
+        for (int j=1;j<10;j++) {
+            printf("%02d ", i*j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
