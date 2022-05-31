d843d67775923b7552551c70551188430bd9abc1
karintou8710 <karintou8710@gmail.com>
2022-06-01 01:37:51 +0900

    Add some files for selfhost

---
+++ selfhost/src/kcc.h
@@ -0,0 +1,321 @@
+/* セルフホスト用の設定 */
+
+typedef long size_t;
+typedef int bool;
+enum BOOL {
+    true = 1,
+    false = 0
+};
+void *NULL = 0;
+int stderr = 2;
+int errno;
+int SEEK_END = 2;
+int SEEK_SET = 0;
+int EXIT_FAILURE = 1;
+int EXIT_SUCCESS = 0;
+
+/* セルフホスト用の設定 終了 */
+
+/*
+ * ~ 構造体 ~
+ * Vector, Type, Token, Var, Node, Function
+ *
+ */
+
+typedef struct Var Var;
+typedef struct Vector Vector;
+typedef struct Type Type;
+typedef struct Token Token;
+typedef struct Node Node;
+typedef struct Function Function;
+typedef struct Initializer Initializer;
+typedef struct GInit_el GInit_el;
+typedef struct Typedef_alias Typedef_alias;
+typedef enum TypeKind TypeKind;
+typedef enum NodeKind NodeKind;
+typedef enum TokenKind TokenKind;
+typedef enum StorageClass StorageClass;
+
+/* ベクターの定義 */
+
+struct Vector {
+    void **body;
+    int len;
+    int capacity;
+};
+
+enum StorageClass {
+    STORAGE_TYPEDEF = 1,
+};
+
+/* 型の定義 */
+enum TypeKind {
+    TYPE_CHAR,
+    TYPE_SHORT,
+    TYPE_INT,
+    TYPE_LONG,
+    TYPE_PTR,
+    TYPE_ARRAY,
+    TYPE_VOID,
+    TYPE_STRUCT,
+    TYPE_ENUM,
+};
+
+struct Type {
+    TypeKind kind;
+    Type *ptr_to;
+    int size;
+    int array_size;
+
+    // struct, enum
+    char *name;
+    Var *member;
+    bool is_forward;
+};
+
+/* トークンの定義 */
+enum TokenKind {
+    TK_NUM = 256,    // number
+    TK_IDENT,        // ident
+    TK_EQ,           // ==
+    TK_NE,           // !=
+    TK_LE,           // <= 260
+    TK_GE,           // >=
+    TK_RSHIFT,       // >>
+    TK_LSHIFT,       // <<
+    TK_ADD_EQ,       // +=
+    TK_SUB_EQ,       // -=
+    TK_MUL_EQ,       // *=
+    TK_DIV_EQ,       // /=
+    TK_MOD_EQ,       // %=
+    TK_AND_EQ,       // &=
+    TK_OR_EQ,        // |=
+    TK_XOR_EQ,       // ^=
+    TK_LSHIFT_EQ,    // <<=
+    TK_RSHIFT_EQ,    // >>=
+    TK_INC,          // ++
+    TK_DEC,          // --
+    TK_RETURN,       // return
+    TK_IF,           // if
+    TK_ELSE,         // else
+    TK_FOR,          // for
+    TK_WHILE,        // while
+    TK_EOF,          // eof
+    TK_TYPE,         // int
+    TK_SIZEOF,       // sizeof
+    TK_STRING,       // string
+    TK_CONTINUE,     // continue
+    TK_BREAK,        // break
+    TK_LOGICAL_AND,  // &&
+    TK_LOGICAL_OR,   // ||
+    TK_ARROW,        // ->
+    TK_TYPEDEF,      // typedef
+    TK_VARIADIC,     // ...
+    TK_INCLUDE,      // include
+};
+
+struct Token {
+    TokenKind kind;         //
+    Type *type;             //
+    Token *next;            //
+    long val;               //
+    char *str;              //
+    int str_literal_index;  //
+    int len;                //
+
+    bool is_standard;  // 標準ヘッダーファイルのインクルード
+};
+
+/* 変数・定数の定義 */
+struct Var {
+    Var *next;        // 次の変数かNULL
+    char *name;       // 変数の名前
+    int len;          // 名前の長さ
+    int offset;       // RBPからのオフセット
+    int next_offset;  // ローカルスコープでのオフセットを管理
+    Type *type;       // 型情報
+    long val;         // 定数の場合は値を持つ
+
+    bool is_global;
+    bool is_only_type;
+    Vector *ginit;  // GInit_elのVector
+};
+
+/* ノードの定義 */
+enum NodeKind {
+    ND_ADD,            // +
+    ND_SUB,            // -
+    ND_MUL,            // *
+    ND_DIV,            // /
+    ND_MOD,            // %
+    ND_ASSIGN,         // =
+    ND_EQ,             // ==
+    ND_NE,             // !=
+    ND_LT,             // <
+    ND_LE,             // <=
+    ND_LSHIFT,         // <<
+    ND_RSHIFT,         // >>
+    ND_AND,            // &
+    ND_OR,             // |
+    ND_XOR,            // ^
+    ND_NOT,            // ~
+    ND_VAR,            // local var
+    ND_NUM,            // num
+    ND_RETURN,         // return
+    ND_IF,             // if
+    ND_ELSE,           // else
+    ND_FOR,            // for
+    ND_WHILE,          // while
+    ND_BLOCK,          // block {}
+    ND_CALL,           // call
+    ND_ADDR,           // & アドレス
+    ND_DEREF,          // * ポインタ
+    ND_STRING,         // string literal
+    ND_CONTINUE,       // continue
+    ND_BREAK,          // break
+    ND_LOGICALNOT,     // !
+    ND_LOGICAL_AND,    // &&
+    ND_LOGICAL_OR,     // ||
+    ND_SUGER,          // 糖衣構文
+    ND_NULL,           // 何もしない
+    ND_STRUCT_MEMBER,  // struct member
+    ND_TERNARY,        // 3項演算子
+    ND_CAST,           // キャスト
+    ND_STMT_EXPR,      // stmt in expr
+};
+
+struct Node {
+    NodeKind kind;
+    Node *lhs;          // 左辺
+    Node *rhs;          // 右辺
+    long val;           // ND_NUM ND_STRINGの時に使う
+    Var *var;           // kindがND_VARの場合のみ使う
+    char *fn_name;      //
+    char *str_literal;  // ND_STRINGのときに使う
+    Vector *args;       //
+    Vector *stmts;      //
+    Type *type;         // 型
+
+    // if (cond) then els
+    // while (cond) body
+    // for (init;cond;inc) body
+    Node *cond;
+    Node *then;
+    Node *els;
+    Node *body;
+    Node *init;
+    Node *inc;
+};
+
+/* 関数型の定義 */
+struct Function {
+    char *name;
+    Node *body;
+    Var *params;
+    Var *locals;
+    int stack_size;
+
+    Type *ret_type;  // return_type
+
+    bool is_prototype;
+    bool is_variadic;
+};
+
+struct Initializer {
+    Type *type;
+    Var *var;  // 代入先の変数を格納
+
+    Node *expr;
+    Initializer *children;
+    int len;
+};
+
+// グローバル変数の初期化式
+struct GInit_el {
+    long val;
+    char *str;
+    int len;
+};
+
+struct Typedef_alias {
+    char *name;
+    Type *type;
+};
+
+// parse.c
+void program();
+Function *find_func(char *name);
+
+// util.c
+int is_alpha(char c);
+int is_alnum(char c);
+void str_advanve(char **p);
+void next_token();
+Token *get_nafter_token(int n);
+bool startsWith(char *p, char *q);
+void error_at(char *loc, char *msg);
+void error(char *fmt, ...);
+char *my_strndup(char *s, size_t n);
+void swap(void **p, void **q);
+void *memory_alloc(size_t size);
+void copy_func(Function *to, Function *from);
+
+// debug.c
+void print_node_kind(NodeKind kind);
+void print_token_kind(TokenKind kind);
+void print_type_kind(TypeKind kind);
+void debug_var(Var *var);
+void debug_type(Type *ty, int depth);
+void debug_node(Node *node, char *pos, int depth);
+void debug_token(Token *t);
+void debug_initializer(Initializer *init, int depth);
+void debug(char *fmt, ...);
+
+// vector.c
+Vector *new_vec();
+void vec_push(Vector *v, void *elem);
+void *vec_pop(Vector *v);
+void *vec_last(Vector *v);
+bool vec_contains(Vector *v, void *elem);
+bool vec_union1(Vector *v, void *elem);
+void vec_concat(Vector *to, Vector *from);
+void *vec_delete(Vector *v, int index);
+
+// codegen.c
+void codegen();
+
+// token.c
+Token *tokenize(char *p);
+
+// type.c
+Type *new_type(TypeKind tykind);
+Type *new_ptr_type(Type *ptr_to);
+Type *new_array_type(Type *ptr_to, int size);
+void add_type(Node *node);
+int sizeOfType(Type *ty);
+bool is_integertype(TypeKind kind);
+TypeKind large_numtype(Type *t1, Type *t2);
+bool can_type_cast(Type *ty, TypeKind to);
+int array_base_type_size(Type *ty);
+bool is_same_type(Type *ty1, Type *ty2);
+
+// preprocess.c
+Token *preprocess(Token *tok);
+
+// main.c
+char *read_file(char *path);
+
+// グローバル変数
+Vector *string_literal;
+Var *globals;
+Token *token;      // tokenは単方向の連結リスト
+char *user_input;  // 入力プログラム
+char *file_name;
+Vector *funcs;         // Function型のVector
+int label_if_count;    // ifのラベル
+int label_loop_count;  // forとwhileのラベル
+Vector *struct_global_lists;
+Vector *struct_local_lists;  // 既出の構造体
+Vector *enum_global_lists;
+Vector *enum_local_lists;  // 既出の列挙型
+Vector *typedef_alias;     // Type_alias
